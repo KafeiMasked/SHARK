9efd4db53c9230414ec98627486ed58a5eccc623
Ze-Ning <zeningong@berkeley.edu>
2020-12-05 22:49:26 -0700

    sharks

--- src/Info.js
+++ src/Info.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react'
 
 const Info = () => {
     return(
-        <h1 id="title"> WOOOPEEE sharks
-            <h6 id= "parHeading"> This is a type of shark </h6>
-                <p id="sharkInfo">sharks are pretty cool cuz they sell them at ikea.</p>
-            <h6 id= "parHeading"> This is a type of shark </h6>
-                <p id="sharkInfo">sharks are pretty cool cuz they sell them at ikea.</p>
-        </h1>
+            <h1 id="title"> Some-fin Interesting About Sharks
+                <h6 id= "parHeading"> Jaw-some Shark Facts </h6>
+                    <p id="sharkInfo">1. Sharks do not have bones.</p>
+                    <p id="sharkInfo">2. The whale shark is the largest fish in the ocean.</p>
+                    <p id="sharkInfo">3. Shark embryos attack each other.</p>
+                    <p id="sharkInfo">4. Sharks have been around for about 450 million years.</p>
+                    <p id="sharkInfo">5. Sharks mate at their birthplace.</p>
+                <h6 id= "parHeading"> Types of Sharks </h6>
+                    <h6 id="sharkHeading">Mako Shark</h6>
+                        <p id="sharkInfo">The Mako Shark is known for its speed, reaching speeds of 31 miles per hour. That speed helps them chase other speedy fish, as makos hunt swordfish and sailfish. They can even leap 20 feet out of the water, and sometimes land in boats. </p>
+                    <h6 id="sharkHeading">Blahaj Shark</h6>
+                        <p id="sharkInfo">Mainly in the Gulf of Finland, the Blahaj is known for its soft and gentle nature in comparison to other sharks. Somehow it is also able to bake cookies and make memes.</p>
+                    <h6 id="sharkHeading">Dwarf Lanternshark</h6>
+                        <p id="sharkInfo">This shark is the smallest shark in the world. It generally grouws to about 6 inches in lengh, and are dark brown collored. </p>
+                    <h6 id="sharkHeading">Hammerhead Shark</h6>
+                        <p id="sharkInfo">Hammerheads are one of the few shark species that gives live birth. To find prey, they can detect electrical signals from bodies in the ocean.</p>
+                    <h6 id="sharkHeading">Sawshark</h6>
+                        <p id="sharkInfo">As the name suggests, this shark has a long saw-like rostrum, used to strike and cut their prey. However, they are harmless to humans!</p>
+            </h1>
     )
 
 }
